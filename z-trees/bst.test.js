9c27e695a1f97e70fccab179db29779ca126fdfa
Alex Mazhavy <alexander.mazhavy@gmail.com>
2018-08-17 16:52:14 -0400

    Quick Fix

---
+++ z-trees/bst.test.js
@@ -0,0 +1,35 @@
+describe('binary search tree', () => {
+    test('BinaryTree class is constructor', () => {
+
+    })
+    test('BinaryTree instance is an object', () => {
+
+    })
+    test('BinaryTree instance has a property "root" ', () => {
+
+    })
+    test('Node class is constructor of BST nodes', () => {
+
+    })
+    test('Node instance has 3 properties: val, left, right', () => {
+
+    })
+    test('BinaryTree has a method "insert" ', () => {
+
+    })
+    test('If no nodes in tree, inserted node should become a root ', () => {
+
+    })
+    test('If root exist, we insert a node to the left if node.val smaller or equal and root left === null', () => {
+
+    })
+    test('If root exist, we insert a node to the right if node.val is greater and root.right === null', () => {
+
+    })
+    test('If root exist, we insert a node to the right if node.val is greater and root.right === null', () => {
+
+    })
+    test('If root has children we traverse a tree until we find the condition', () => {
+
+    })
+}) 
